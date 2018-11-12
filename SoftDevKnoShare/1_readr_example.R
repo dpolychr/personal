2d8fe54fd33ae5797668190834dc24607a119493
dpolychr <dpolychr@gmail.com>
2018-11-12 14:25:55 +0000

    Add readr example

---
+++ SoftDevKnoShare/1_readr_example.R
@@ -0,0 +1,26 @@
+# Load the readr package
+library(readr)
+
+# Import the csv file: pools
+url_csv <- "http://s3.amazonaws.com/assets.datacamp.com/production/course_1478/datasets/swimming_pools.csv"
+pools = read_csv(url_csv)
+
+# Import the txt file: potatoes
+url_delim <- "http://s3.amazonaws.com/assets.datacamp.com/production/course_1478/datasets/potatoes.txt"
+potatoes = read_tsv(url_delim)
+
+# Specify column names
+properties <- c("area", "temp", "size", "storage", "method",
+                "texture", "flavor", "moistness")
+
+# Import all data, but force all columns to be character: potatoes_char
+potatoes_char <- read_tsv(url_delim, col_types = "cccccccc", col_names = properties, skip = 1)
+
+# https URL to the swimming_pools csv file.
+url_csv <- "https://s3.amazonaws.com/assets.datacamp.com/production/course_1478/datasets/swimming_pools.csv"
+
+# Import the file using read.csv(): pools1
+pools1 = read.csv(url_csv, header = TRUE)
+
+# Import the file using read_csv(): pools2
+pools2 = read_csv(url_csv)
